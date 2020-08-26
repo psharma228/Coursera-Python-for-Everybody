4522ca9dbaca1cf5a63f80252c38837f0d0ed4f6
Pulkit Sharma <56049972+psharma228@users.noreply.github.com>
2020-08-26 15:49:18 +0530

    Add files via upload

---
+++ Python Data Structures/Week 3/Assignment7.2.py
@@ -0,0 +1,25 @@
+# Write a program that prompts for a file name, then opens that file and reads through the file, 
+# looking for lines of the form:
+# X-DSPAM-Confidence:    0.8475
+# Count these lines and extract the floating point values from each of the lines and compute the 
+# average of those values and produce an output as shown below.
+# You can download the sample data at http://www.pythonlearn.com/code/mbox-short.txt when you are 
+# testing below enter mbox-short.txt as the file name.
+# Average spam confidence: 0.750718518519
+
+
+
+# Use the file name mbox-short.txt as the file name
+fname = raw_input("Enter file name: ")
+fh = open(fname)
+count = 0
+y=0.0
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence:") : continue
+    count = count+1
+    t = line.find('0')
+    x=float(line[t:])
+    y = y + x
+    avg = y / count
+    
+print ("Average spam confidence:",avg)
